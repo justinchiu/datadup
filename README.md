5cbb13e055d9dd92d14c1ade9dfb4a7bdf4ba7ff
Justin Chiu <chiu.justin.t@gmail.com>
2024-02-20 11:29:39 -0500

    just use infinigram

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Check if the test set of your data is duplicated in your pretraining data :skull
 This should be a bit easier than checking if your training dataset has duplications if your
 test set is small -- you only have to index the test set.
 
+# Recommendation: Just use infinigram API.
+See `scripts/query_infinigram.py`.
+
 [Suffix array implementation by N Carlini](https://github.com/google-research/deduplicate-text-datasets/tree/master).
---
+++ scripts/query_infinigram.py
@@ -0,0 +1,56 @@
+import datasets
+import requests
+import json
+from pathlib import Path
+
+url = "https://api.infini-gram.io/"
+pg19 = datasets.load_dataset(
+    "pg19",
+    split="test",
+    trust_remote_code=True,
+)
+
+counts = []
+bad_idxs = []
+bad_docs = []
+repeat_queries = []
+repeat_counts = []
+for i, ex in enumerate(pg19):
+    text = ex["text"]
+    lines = text.replace("\n\n", "\n").replace("\n\n", "\n").split("\n")
+    # find first line with > 10 words
+    for line in lines:
+        if len(line.split()) > 10:
+            query = line
+            break
+
+    payload = {
+        "corpus": "v4_piletrain_llama",
+        "query_type": "count",
+        "query": query,
+    }
+
+    # Headers to specify that we are sending JSON data
+    headers = {"Content-Type": "application/json"}
+
+    # Sending the POST request
+    response = requests.post(url, json=payload, headers=headers)
+
+    count = response.json()["count"]
+    counts.append(count)
+    if count > 0:
+        bad_idxs.append(i)
+        bad_docs.append(ex)
+        repeat_queries.append(query)
+        repeat_counts.append(count)
+
+output = {
+    "bad_idxs": bad_idxs,
+    "bad_docs": bad_docs,
+    "repeat_queries": repeat_queries,
+    "repeat_counts": repeat_counts,
+}
+
+path = Path("duplicates.json")
+with path.open("w") as f:
+    json.dump(output, f)
